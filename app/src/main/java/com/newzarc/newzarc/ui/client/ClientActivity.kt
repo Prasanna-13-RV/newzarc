bda4a39c1f9abf51f0607977b0c177a0b22071cc
Prasanna <prasannavelmurugan0200@gmail.com>
2023-06-03 10:44:00 +0530

    changes

---
+++ app/src/main/java/com/newzarc/newzarc/ui/client/ClientActivity.kt
@@ -0,0 +1,42 @@
+package com.newzarc.newzarc.ui.client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.newzarc.newzarc.R
+import com.newzarc.newzarc.databinding.ActivityClientBinding
+import com.newzarc.newzarc.ui.client.fragment.AccountUserFragment
+import com.newzarc.newzarc.ui.client.fragment.HomeFragment
+import com.newzarc.newzarc.ui.client.fragment.NewsViewFragment
+
+class ClientActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityClientBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityClientBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.news -> replaceFragment(NewsViewFragment())
+                R.id.account -> replaceFragment(AccountUserFragment())
+            }
+
+            true
+        }
+
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, fragment)
+        fragmentTransaction.commit()
+    }
+}
